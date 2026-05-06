1c2eafb9012b4191f95437e2dbc773fe74424915
bsratzlaff-del <bsratzlaff@gmail.com>
2026-05-06 14:16:49 -0400

    create powershell command files

--- start_app.ps1
+++ start_app.ps1
@@ -1,1 +1,5 @@
 Start-Job -ScriptBlock { & "$env:LOCALAPPDATA\Android\Sdk\emulator\emulator.exe" -avd CoffeeShop }
+
+& "$env:LOCALAPPDATA\Android\Sdk\emulator\emulator.exe" -avd CoffeePhone -no-snapshot-load
+
+dotnet build -t:Run -f net10.0-android -p:AdbTarget="-e"
